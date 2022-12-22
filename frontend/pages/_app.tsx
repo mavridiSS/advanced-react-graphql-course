961a231688a11ce803a7091d6ff327faefb679ec
Georgi Mavridis <georgimavridis21@gmail.com>
2022-12-22 11:29:39 +0200

    header and nav components

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,29 @@
 import React from "react";
 import type { AppProps } from "next/app";
+import Router from "next/router";
+
+import NProgress from "nprogress";
+import Page from "../components/Page";
+import "../components/styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+
+Router.events.on("routeChangeComplete", () => {
+  NProgress.done();
+});
+
+Router.events.on("routeChangeError", () => {
+  NProgress.done();
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  React.useEffect(() => {});
+
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
 }
